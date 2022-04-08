dc22385fea47507664826600e8fcaca24e4590b1
Ayush Patel <55711453+ayushpatel996@users.noreply.github.com>
2022-04-08 16:49:27 +0530

    commit one

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -52,7 +52,7 @@ function Header() {
 
             <div className="logo">
                 <h1>
-                    <Link to="/">{isAdmin ? 'Admin' : 'Rent Anything'}</Link>
+                    <Link to="/">{isAdmin ? 'Admin' : 'NFT Marketplace'}</Link>
                 </h1>
             </div>
 
